7f771a82fff5cf4f7b2010b8fa3d6131824295c1
Eduardo Fuentes <eduardo.fuentes@viaro.net>
2024-10-23 08:42:18 -0600

    Se creo una nueva forma de busqueda y devolución de datos para el listado de productos utilizando objetos DTO

---
+++ Frontend/src/app/dto/producto-dto.ts
@@ -0,0 +1,22 @@
+export class ProductoDTO {
+    idProducto: number;
+    codProducto: string;
+    serie: string;
+    nombre: string;
+    precioVenta: number;
+    precioSugerido: number;
+    precioCompra: number;
+    porcentajeGanancia: number;
+    imagen: string;
+    descripcion: string;
+    link: string;
+    fechaVencimiento: Date;
+    fechaIngreso: Date;
+    fechaRegistro: Date;
+    stock: number;
+    marcaProducto: string;
+    tipoProducto: string;
+    estado: string;
+    idEstado: number;
+    proveedor: string;
+}
